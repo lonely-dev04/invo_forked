433a2532b556a586163ec8c23eb8fc413d2d2ff5
Thejesh Bhagavanth <thejeshbhagavanth@gmail.com>
2023-09-22 16:05:23 +0530

    committed

--- invo/settings.py
+++ invo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ou=49f7o%m5++slsgo5-cy&b17fw6p#p#f$7s!5e+90%#en^i1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
