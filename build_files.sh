4ef4ae55c8c81a94be6784248794a3cac0c7e52f
Thejesh Bhagavanth <thejeshbhagavanth@gmail.com>
2023-09-23 09:21:58 +0530

    committed

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,6 @@
-pip install -r requirements.txt 
+
+pip install -r requirements.txt
+
+python manage.py collectstatic
+
+python manage.py migrate
--- invo/settings.py
+++ invo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ou=49f7o%m5++slsgo5-cy&b17fw6p#p#f$7s!5e+90%#en^i1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
